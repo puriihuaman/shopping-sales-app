1460b68e7b54a77e084dba36b2e1f4a38e149bac
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-22 10:52:46 -0500

    feat: add profile schema

---
+++ backend/src/schemas/profile.schemas.js
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+const profileSchema = z.object({
+	code: z.string().uuid().trim(),
+	profile: z
+		.string({
+			required_error: 'Profile profile must be a string',
+			invalid_type_error: 'Profile profile is required',
+		})
+		.trim()
+		.min(2, { message: 'Must be 2 or more characters long' }),
+});
+
+const validateProfile = (profile) => {
+	return profileSchema.safeParse(profile);
+};
+
+const validatePartialProfile = (profile) => {
+	return profileSchema.safeParse(profile);
+};
+
+export { validatePartialProfile, validateProfile };
+
+// code - profile
