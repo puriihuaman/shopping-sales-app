5cbe3238de039a00982efa553b274412135e8b4d
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-24 20:03:05 -0500

    feat: add cors configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import { PORT } from './config/config.js';
 import { profileRouter } from './routes/profile.routes.js';
 import { userRouter } from './routes/user.routes.js';
 import { authRouter } from './routes/auth.routes.js';
+import { corsMiddlewares } from './middlewares/cors.middleware.js';
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(json());
+app.use(corsMiddlewares());
 
 app.use('/api/login', authRouter);
 app.use('/api/profiles', profileRouter);
