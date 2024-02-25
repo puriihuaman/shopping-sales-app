3d50c31749af02ec4a22e376ec40cb2c6ff35012
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-24 19:53:08 -0500

    feat: add font configuration in tailwindcss configuration file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,31 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+				heading: [
+					'Inter',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'ui-sans-serif',
+					'system-ui',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+				],
+			},
+			fontWeight: {
+				normal: '400',
+				bold: '700',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require('tailwindcss'), require('autoprefixer')],
 };
