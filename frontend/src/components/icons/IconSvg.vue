460d9369e65a7f09bd1a4104fb8dc0773392339b
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-24 19:50:11 -0500

    feat: add IconSvg component

---
+++ frontend/src/components/icons/IconSvg.vue
@@ -0,0 +1,34 @@
+<template>
+	<span class="icon">
+		<svg class="icon-svg" :class="` ${otherClass ?? 'icon-sm'}`">
+			<use :href="path + iconID"></use>
+		</svg>
+	</span>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	iconID: string;
+	otherClass?: string;
+}>();
+
+const path = '/assets/icons/svg-icons.svg#';
+</script>
+
+<style scoped lang="scss">
+.icon {
+	@apply flex justify-center items-center;
+}
+.icon-svg {
+	@apply block text-current fill-current;
+}
+.icon-sm {
+	@apply w-6 h-6;
+}
+.icon-md {
+	@apply w-8 h-8;
+}
+.icon-lg {
+	@apply w-12 h-12;
+}
+</style>
