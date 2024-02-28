fc4ee172e5c95f03de3fb7b1b181eebb488d94f4
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-28 12:23:40 -0500

    fix: improve responses in profile controller

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -1,7 +1,6 @@
-import {
-	validatePartialProfile,
-	validateProfile,
-} from '../schemas/profile.schemas.js';
+import { validatePartialProfile, validateProfile } from '../schemas/index.js';
+
+import { ClientError, catchedAsync, response } from '../utils/index.js';
 
 export class ProfileController {
 	#profileModel;
@@ -10,7 +9,17 @@ export class ProfileController {
 		this.#profileModel = profileModel;
 	}
 
-	getAll = async (req, res) => {
+	// + después
+	getAllProfiles = catchedAsync(async (req, res) => {
+		const { message, obtainedProfile, status, dataRecords } =
+			await this.#profileModel.getAll();
+
+		response(res, status, dataRecords, message);
+	});
+
+	// + Antes
+	/*
+	getAllProfiles = async (req, res, next) => {
 		try {
 			const { message, obtainedProfile, dataRecords, error } =
 				await this.#profileModel.getAll();
@@ -32,7 +41,20 @@ export class ProfileController {
 			});
 		}
 	};
+	*/
 
+	getById = catchedAsync(async (req, res) => {
+		const { id } = req.params;
+
+		const { message, obtainedProfile, status, dataRecords } =
+			await this.#profileModel.getById({
+				id,
+			});
+
+		response(res, status, dataRecords, message);
+	});
+
+	/*
 	getById = async (req, res) => {
 		const { id } = req.params;
 
@@ -57,95 +79,145 @@ export class ProfileController {
 			});
 		}
 	};
+	*/
 
-	createProfile = async (req, res) => {
+	createProfile = catchedAsync(async (req, res) => {
 		const { body } = req;
-		const validationResponse = validateProfile(body);
+		const { error, success, data } = validateProfile(body);
 
-		if (validationResponse.error && !validationResponse.success) {
-			return res.status(400).json({ error: validationResponse.error.message });
+		if (error && !success) {
+			throw new ClientError('invalid fields', 400, error.errors[0].message);
 		}
 
-		try {
-			const { message, insertedProfile, dataRecords, error } =
-				await this.#profileModel.createProfile({
-					input: validationResponse.data,
-				});
+		const { message, insertedProfile, status, dataRecords } =
+			await this.#profileModel.createProfile({
+				input: data,
+			});
 
-			if (!insertedProfile && dataRecords.length === 0) {
-				throw new Error(message);
+		response(res, status, dataRecords, message);
+	});
+
+	/*
+		createProfile = async (req, res) => {
+			const { body } = req;
+			const validationResponse = validateProfile(body);
+
+			if (validationResponse.error && !validationResponse.success) {
+				return res.status(400).json({ error: validationResponse.error.message });
 			}
 
-			return res.status(201).json({
-				message,
-				data: dataRecords,
-				errors: error,
-			});
-		} catch (error) {
-			return res.status(409).json({
-				message: error.message,
-				data: [],
-				errors: error,
-			});
-		}
-	};
+			try {
+				const { message, insertedProfile, dataRecords, error } =
+					await this.#profileModel.createProfile({
+						input: validationResponse.data,
+					});
 
-	deleteProfile = async (req, res) => {
+				if (!insertedProfile && dataRecords.length === 0) {
+					throw new Error(message);
+				}
+
+				return res.status(201).json({
+					message,
+					data: dataRecords,
+					errors: error,
+				});
+			} catch (error) {
+				return res.status(409).json({
+					message: error.message,
+					data: [],
+					errors: error,
+				});
+			}
+		};
+	*/
+
+	deleteProfile = catchedAsync(async (req, res) => {
 		const { id } = req.params;
 
-		try {
-			const { message, deletedProfile, dataRecords, error } =
-				await this.#profileModel.deleteProfile({ id });
+		const { message, deletedProfile, status, dataRecords } =
+			await this.#profileModel.deleteProfile({ id });
 
-			if (!deletedProfile && dataRecords.length === 0) {
-				throw new Error(message);
+		response(res, status, dataRecords, message);
+	});
+
+	/*
+		deleteProfile = async (req, res) => {
+			const { id } = req.params;
+
+			try {
+				const { message, deletedProfile, dataRecords, error } =
+					await this.#profileModel.deleteProfile({ id });
+
+				if (!deletedProfile && dataRecords.length === 0) {
+					throw new Error(message);
+				}
+
+				return res.status(200).json({
+					message,
+					data: dataRecords,
+					errors: error,
+				});
+			} catch (error) {
+				return res.status(404).json({
+					message: error.message,
+					data: [],
+					errors: error,
+				});
 			}
+		};
+	*/
 
-			return res.status(200).json({
-				message,
-				data: dataRecords,
-				errors: error,
-			});
-		} catch (error) {
-			return res.status(404).json({
-				message: error.message,
-				data: [],
-				errors: error,
-			});
-		}
-	};
-
-	updateProfile = async (req, res) => {
+	updateProfile = catchedAsync(async (req, res) => {
 		const { id } = req.params;
 
-		const validationResponse = validatePartialProfile(req.body);
+		const { error, success, data } = validatePartialProfile(req.body);
 
-		if (validationResponse.error && !validationResponse.success) {
-			return res.status(400).json({ error: validationResponse.error.message });
+		if (error && !success) {
+			throw new ClientError('invalid fields', 400, error.errors[0].message);
 		}
 
-		try {
-			const { message, updatedProfile, dataRecords, error } =
-				await this.#profileModel.updateProfile({
-					id,
-					input: validationResponse.data,
-				});
+		const { message, updatedProfile, status, dataRecords } =
+			await this.#profileModel.updateProfile({
+				id,
+				input: data,
+			});
 
-			if (!updatedProfile && dataRecords.length === 0) {
-				throw new Error(message);
+		response(res, status, dataRecords, message);
+	});
+
+	/*
+		updateProfile = async (req, res) => {
+			const { id } = req.params;
+
+			const validationResponse = validatePartialProfile(req.body);
+
+			if (validationResponse.error && !validationResponse.success) {
+				return res.status(400).json({ error: validationResponse.error.message });
 			}
 
-			return res.status(200).json({
-				message,
-				data: dataRecords,
-				errors: error,
-			});
-		} catch (error) {
-			return res.status(404).json({
-				message: error.message,
-				data: [],
-				errors: error,
-			});
-		}
-	};
+			try {
+				const { message, updatedProfile, dataRecords, error } =
+					await this.#profileModel.updateProfile({
+						id,
+						input: validationResponse.data,
+					});
+
+				if (!updatedProfile && dataRecords.length === 0) {
+					throw new Error(message);
+				}
+
+				return res.status(200).json({
+					message,
+					data: dataRecords,
+					errors: error,
+				});
+			} catch (error) {
+				return res.status(404).json({
+					message: error.message,
+					data: [],
+					errors: error,
+				});
+			}
+		};
+	*/
 }
