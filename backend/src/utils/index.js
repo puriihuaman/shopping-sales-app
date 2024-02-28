58a863190d29ae2dbf1185a716b896cc20dbe037
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-28 12:36:29 -0500

    feat: add barrel pattern implementation in utils

---
+++ backend/src/utils/index.js
@@ -0,0 +1,6 @@
+import { catchedAsync } from './catched-async.js';
+import { ClientError, ServerError } from './errors.js';
+import { responseError } from './response-error.js';
+import { response } from './response.js';
+
+export { ClientError, ServerError, catchedAsync, response, responseError };
