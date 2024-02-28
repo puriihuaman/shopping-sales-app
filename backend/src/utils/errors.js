f99da4b6ff191b5ada9ee0a3f19ae2c514b07695
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-28 12:28:41 -0500

    feat: add custom error handler

---
+++ backend/src/utils/errors.js
@@ -0,0 +1,19 @@
+class ClientError extends Error {
+	constructor(message, statusCode = 400, details = '') {
+		super(message);
+		this.statusCode = statusCode;
+		this.details = details;
+	}
+}
+
+class AuthClientError extends Error {}
+
+class ServerError extends Error {
+	constructor(message, statusCode, details = '') {
+		super(message);
+		this.statusCode = statusCode;
+		this.details = details;
+	}
+}
+
+export { AuthClientError, ClientError, ServerError };
