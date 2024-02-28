554adf0d0ea86d80bd28df1a0e80131b006e929a
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-28 12:20:57 -0500

    fixbug: improve error handling in profile model

--- backend/src/models/profile.model.js
+++ backend/src/models/profile.model.js
@@ -1,5 +1,6 @@
-import { pool } from '../databases/connection.js';
 import { randomUUID } from 'node:crypto';
+import { pool } from '../databases/connection.js';
+import { ClientError, ServerError } from '../utils/index.js';
 
 export class ProfileModel {
 	static async getAll() {
@@ -9,24 +10,30 @@ export class ProfileModel {
 
 		try {
 			const result = await client.query(query);
-			if (result.rowCount === 0 && result.rows.length === 0) {
+
+			if (result.rowCount === 0) {
+				// error al obtener profiles
 				// no se pudo obtener los perfiles
-				throw new Error('could not get profiles');
+				throw new ClientError(
+					'error getting profiles',
+					400,
+					'could not get profiles'
+				);
 			}
 
 			return {
 				message: 'profiles successfully obtained',
 				obtainedProfile: true,
+				status: 200,
 				dataRecords: result.rows,
-				error: null,
 			};
 		} catch (error) {
-			return {
-				message: error.message,
-				obtainedProfile: false,
-				dataRecords: [],
-				error,
-			};
+			if (error instanceof ClientError) {
+				throw error;
+			} else {
+				// Otro tipo de error inesperado
+				throw new ServerError('Internal server error', 500);
+			}
 		} finally {
 			await client.end();
 		}
@@ -40,23 +47,28 @@ export class ProfileModel {
 		try {
 			const result = await client.query(selectionQuery, [id]);
 
-			if (result.rowCount === 0 && result.rows.length === 0) {
-				throw new Error('could not get profile');
+			if (result.rowCount === 0) {
+				// no se encontró ningún perfil con ese ID
+				throw new ClientError(
+					'invalid ID',
+					404,
+					'no profile found with that ID'
+				);
 			}
 
 			return {
 				message: 'profile successfully obtained',
 				obtainedProfile: true,
+				status: 200,
 				dataRecords: result.rows[0],
-				error: null,
 			};
 		} catch (error) {
-			return {
-				message: error.message,
-				obtainedProfile: false,
-				dataRecords: [],
-				error,
-			};
+			if (error instanceof ClientError) {
+				throw error;
+			}
+
+			// Otro tipo de error inesperado
+			throw new ServerError('Internal server error', 500);
 		} finally {
 			await client.end();
 		}
@@ -73,40 +85,36 @@ export class ProfileModel {
 		try {
 			const result = await client.query(insertQuery, [idProfile, profile]);
 
-			if (result.rowCount !== 1 && result.command !== 'INSERT') {
-				throw new Error('could not insert profile');
-
-				// return {
-				// 	insertedProfile: false,
-				// 	dataRecords: [],
-				// 	error: 'could not insert profile', //No se pudo insertar el perfil
-				// };
+			if (result.rowCount === 0) {
+				// no se pudo registrar el perfil
+				throw new ClientError(
+					'error registering',
+					400,
+					'profile could not be registered'
+				);
 			}
 
 			return {
-				message: 'profile registered successfully', // perfil registrado exitosamente
+				message: `${profile} profile registered successfully`,
 				insertedProfile: true,
+				status: 201,
 				dataRecords: result.rows[0],
-				error: null,
 			};
 		} catch (error) {
-			if (error.code === '23505' && error.constraint === 'uk_profiles') {
-				return {
-					message:
-						'the profile already exists. Please choose another profile name',
-					insertedProfile: false,
-					dataRecords: [],
-					error,
-				};
-				// El perfil ya existe. Por favor, elija otro nombre de perfil
+			if (error instanceof ClientError) {
+				throw error;
+			} else if (error.code === '23505' && error.constraint === 'uk_profiles') {
+				// el perfil '' ya existe
+				// ya existe el perfil. Por favor, intente con otro nombre de perfil
+				throw new ClientError(
+					`${profile} profile already exists`,
+					409,
+					'the profile already exists. Please try another profile name'
+				);
+			} else {
+				// error al registrar perfil
+				throw new ServerError('error when registering profile', 500, '');
 			}
-
-			return {
-				message: 'error inserting profile', // Error al insertar el perfil
-				insertedProfile: false,
-				dataRecords: [],
-				error,
-			};
 		} finally {
 			await client.end();
 		}
@@ -121,29 +129,29 @@ export class ProfileModel {
 		try {
 			const result = await client.query(deleteQuery, [id]);
 
-			if (result.rowCount === 0 && result.rows.length === 0) {
-				throw new Error('could not delete profile');
-				// return {
-				// 	message: 'error deleting profile',
-				// 	deletedProfile: false,
-				// 	dataRecords: [],
-				// 	error: 'could not delete profile', //No se pudo eliminar el perfil
-				// };
+			if (result.rowCount === 0) {
+				// id invalido
+				// no se encontró ningún perfil con ese ID
+				throw new ClientError(
+					'invalid ID',
+					404,
+					'no profile found with that ID'
+				);
 			}
 
 			return {
 				message: 'profile deleted successfully',
 				deletedProfile: true,
+				status: 200,
 				dataRecords: result.rows[0],
-				error: null,
 			};
 		} catch (error) {
-			return {
-				message: error.message,
-				deletedProfile: false,
-				dataRecords: [],
-				error,
-			};
+			if (error instanceof ClientError) {
+				throw error;
+			}
+
+			// Otro tipo de error inesperado
+			throw new ServerError('Internal server error', 500, '');
 		} finally {
 			await client.end();
 		}
@@ -159,25 +167,45 @@ export class ProfileModel {
 
 		try {
 			const result = await client.query(updateQuery, [profile, id]);
-			console.log(result);
 
-			if (result.rowCount === 0 && result.rows.length === 0) {
-				throw new Error('Could not update profile');
+			if (result.rowCount === 0) {
+				// id invalido
+				// no se encontró ningún perfil con ese ID
+				throw new ClientError(
+					'invalid ID',
+					404,
+					'no profile found with that ID'
+				);
+			} else if (result.rows[0].profile === profile) {
+				// El perfil '' ya está registrado.
+				// El nuevo perfil es igual al perfil existente, no es necesario actualizar
+				throw new ClientError(
+					`the ${profile} profile already exists`,
+					400,
+					'New profile is the same as the existing one'
+				);
 			}
 
 			return {
 				message: 'profile updated successfully',
 				updatedProfile: true,
+				status: 200,
 				dataRecords: result.rows[0],
-				error: null,
 			};
 		} catch (error) {
-			return {
-				message: error.message,
-				updatedProfile: false,
-				dataRecords: [],
-				error,
-			};
+			if (error instanceof ClientError) {
+				throw error;
+			} else if (error.code === '23505' && error.constraint === 'uk_profiles') {
+				// El perfil '' ya está registrado.
+				// ya existe el perfil. Por favor, intente con otro nombre de perfil
+				throw new ClientError(
+					`the ${profile} profile already exists`,
+					409,
+					'the profile already exists. Please try another profile name'
+				);
+			} else {
+				throw new ServerError('error updating profile', 500, '');
+			}
 		} finally {
 			await client.end();
 		}
