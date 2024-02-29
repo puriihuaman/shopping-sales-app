0bc80d69bf4783f006ff5b5e4528d3e798fd4b43
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-29 16:15:22 -0500

    fixbug: fix responses in AuthController class

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { tokenSign } from '../helpers/generate-token.js';
 import { comparePassword } from '../helpers/handleBcrypt.js';
+import { AuthClientError, catchedAsync, response } from '../utils/index.js';
 
 export class AuthController {
 	#userModel;
@@ -7,72 +8,24 @@ export class AuthController {
 		this.#userModel = userModel;
 	}
 
-	loginControl = async (req, res) => {
-		try {
-			const { username, password } = req.body;
+	loginControl = catchedAsync(async (req, res) => {
+		const { username, password } = req.body;
 
-			const { message, foundUser, dataRecords, error } =
-				await this.#userModel.login({ username });
+		const { message, foundUser, status, dataRecords } =
+			await this.#userModel.login({ username });
 
-			if (!foundUser && dataRecords.length === 0) {
-				throw new Error(message);
-			}
+		const checkPassword = await comparePassword(password, dataRecords.password);
 
-			const checkPassword = await comparePassword(
-				password,
-				dataRecords.password
+		if (!checkPassword) {
+			throw new AuthClientError(
+				'credentials error',
+				409,
+				'invalid username or password'
 			);
-
-			const tokenSession = await tokenSign(dataRecords);
-
-			if (!checkPassword) {
-				return res.status(409).json({
-					message: 'invalid username or password',
-					data: [],
-					errors: error,
-				});
-			}
-
-			return res.status(200).json({
-				message,
-				data: dataRecords,
-				errors: error,
-				tokenSession,
-			});
-		} catch (error) {
-			return res.status(404).json({
-				message: error.message,
-				data: [],
-				errors: error,
-			});
 		}
-	};
 
-	registerUser = async (req, res) => {
-		const { body } = req;
+		const tokenSession = await tokenSign(dataRecords);
 
-		const { success, error, data } = validateUser(body);
-
-		if (error && !success)
-			return res.status(400).json({ error: JSON.parse(error.message) });
-
-		try {
-			const input = {
-				...data,
-				password: await encryptPassword(data.password),
-			};
-
-			const user = await this.#userModel.createUser({ input });
-
-			return res.status(201).json(user);
-
-			// message,
-			// 	data: dataRecords,
-			// 	errors: error,
-		} catch (error) {
-			return res.status(500).json({
-				message: 'Something goes wrong',
-			});
-		}
-	};
+		response(res, status, dataRecords, message, tokenSession);
+	});
 }
