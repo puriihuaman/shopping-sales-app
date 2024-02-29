6466b3aa8f4e68d2e7c3dd50f01470a977b5fe13
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-02-29 17:41:16 -0500

    feat: add authorization middlewares to profile routes

--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -1,16 +1,42 @@
 import { Router } from 'express';
-import { ProfileController } from '../controllers/profile.controller.js';
-import { ProfileModel } from '../models/profile.model.js';
-import { checkAuth } from '../middlewares/auth.middleware.js';
+
+import { ProfileController } from '../controllers/index.js';
+import { checkAuth, checkProfileAuth } from '../middlewares/auth.middleware.js';
+import { ProfileModel } from '../models/index.js';
 
 const profileRouter = Router();
 
 const profileController = new ProfileController({ profileModel: ProfileModel });
 
-profileRouter.get('/', checkAuth, profileController.getAll);
-profileRouter.get('/:id', checkAuth, profileController.getById);
-profileRouter.post('/', checkAuth, profileController.createProfile);
-profileRouter.delete('/:id', checkAuth, profileController.deleteProfile);
-profileRouter.patch('/:id', checkAuth, profileController.updateProfile);
+profileRouter.get(
+	'/',
+	checkAuth,
+	checkProfileAuth({ profiles: ['administrador'] }),
+	profileController.getAllProfiles
+);
+profileRouter.get(
+	'/:id',
+	checkAuth,
+	checkProfileAuth({ profiles: ['administrador'] }),
+	profileController.getById
+);
+profileRouter.post(
+	'/',
+	checkAuth,
+	checkProfileAuth({ profiles: ['administrador'] }),
+	profileController.createProfile
+);
+profileRouter.delete(
+	'/:id',
+	checkAuth,
+	checkProfileAuth({ profiles: ['administrador'] }),
+	profileController.deleteProfile
+);
+profileRouter.patch(
+	'/:id',
+	checkAuth,
+	checkProfileAuth({ profiles: ['administrador'] }),
+	profileController.updateProfile
+);
 
 export { profileRouter };
