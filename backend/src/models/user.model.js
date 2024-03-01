26187f347f384c7584a75c0d3a703ad00d0d44a2
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-01 11:41:03 -0500

    feat: add the implementation of the methods in the users model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,14 +3,93 @@ import { pool } from '../databases/connection.js';
 import { ClientError, ServerError } from '../utils/index.js';
 
 export class UserModel {
-	static async getAll() {
+	static async getUsers() {
 		const selectionQuery = 'SELECT * FROM users';
 
 		const client = await pool.connect();
-		const result = await client.query(selectionQuery);
 
-		await client.end();
-		return result.rows;
+		try {
+			const result = await client.query(selectionQuery);
+
+			if (result.rowCount === 0) {
+				// error al obtener usuarios
+				// no se pudo obtener los usuarios
+				throw new ClientError(
+					'Error getting users',
+					400,
+					'Could not get users'
+				);
+			}
+
+			return {
+				message: 'Users successfully obtained',
+				obtainedUser: true,
+				status: 200,
+				dataRecords: result.rows,
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				throw error;
+			} else {
+				throw new ServerError('Internal server error', 500);
+			}
+		} finally {
+			await client.end();
+		}
+	}
+
+	static async getUserByUsername({ username }) {
+		const selectionQuery = 'SELECT * FROM users WHERE username = $1';
+
+		const client = await pool.connect();
+
+		try {
+			const result = await client.query(selectionQuery, [username]);
+
+			return {
+				message: 'User successfully obtained',
+				obtainedUser: true,
+				status: 200,
+				dataRecords: result.rows[0],
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				throw error;
+			} else {
+				throw new ServerError('Internal server error', 500);
+			}
+		} finally {
+			await client.end();
+		}
+	}
+
+	static async getUserById({ id }) {
+		const selectionQuery = 'SELECT * FROM users WHERE id_user = $1';
+
+		const client = await pool.connect();
+		try {
+			const result = await client.query(selectionQuery, [id]);
+
+			if (result.rowCount === 0) {
+				// no se encontró ningún usuario con ese ID
+				throw new ClientError('invalid ID', 404, 'No user found with that ID');
+			}
+
+			return {
+				message: 'User successfully obtained',
+				obtainedUser: true,
+				status: 200,
+				dataRecords: result.rows[0],
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				throw error;
+			} else {
+				throw new ServerError('Internal server error', 500);
+			}
+		} finally {
+			await client.end();
+		}
 	}
 
 	static async createUser({ input }) {
@@ -18,7 +97,7 @@ export class UserModel {
 			input;
 
 		const insertQuery =
-			'INSERT INTO users (id_user, name_user, lastname, email, username, password, id_profile) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+			'INSERT INTO users (id_user, name_user, lastname, email, username, password, id_profile) VALUES ($1, $2, $3, $4, LOWER($5), $6, $7) RETURNING *';
 
 		const idUser = randomUUID();
 		const client = await pool.connect();
@@ -29,39 +108,149 @@ export class UserModel {
 				name_user,
 				lastname,
 				email,
-				username,
+				username.toLowerCase(),
 				password,
 				id_profile,
 			]);
 
-			if (result.rowCount === 0 && result.rows.length === 0) {
+			if (result.rowCount === 0) {
 				// no se pudo registrar el usuario
-				throw new Error('user could not be registered');
+				throw new ClientError(
+					'Error registering',
+					500,
+					'User could not be registered'
+				);
 			}
 
 			return {
-				message: 'user registered successfully', // usuario registrado exitosamente
+				message: 'User registered successfully',
 				insertedUser: true,
+				status: 201,
 				dataRecords: result.rows[0],
-				error: null,
 			};
 		} catch (error) {
 			if (error.code === '23505' && error.constraint === 'uk_users') {
-				return {
-					message: 'user already exists. Please choose another username',
-					insertedUser: false,
-					dataRecords: [],
-					error,
-				};
-				// El usuario ya existe. Por favor, elija otro nombre de usuario
+				throw new ClientError(
+					`User with username '${username}' already exists`,
+					409,
+					'The username already exists. Please try another username'
+				);
+			} else {
+				throw new ServerError('Error when registering user', 500, '');
+			}
+		} finally {
+			await client.end();
+		}
+	}
+
+	static async deleteUser({ id }) {
+		const deleteQuery = 'DELETE FROM users WHERE id_user = $1 RETURNING *;';
+
+		const client = await pool.connect();
+
+		try {
+			const result = await client.query(deleteQuery, [id]);
+
+			if (result.rowCount === 0) {
+				// id invalido
+				// no se encontró ningún usuario con ese ID
+				throw new ClientError('Invalid ID', 404, 'No user found with that ID');
 			}
 
 			return {
-				message: 'error inserting user', // Error al insertar el usuario
-				insertedUser: false,
-				dataRecords: [],
-				error,
+				message: 'User deleted successfully',
+				deletedUser: true,
+				status: 200,
+				dataRecords: result.rows[0],
 			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				throw error;
+			}
+
+			// Otro tipo de error inesperado
+			throw new ServerError('Internal server error', 500, '');
+		} finally {
+			await client.end();
+		}
+	}
+
+	static async updateUser({ id, input }) {
+		const updateQuery =
+			'UPDATE users SET name_user = COALESCE($1, name_user), lastname = COALESCE($2, lastname), email = COALESCE($3, email), id_profile = COALESCE($4, id_profile) WHERE id_user = $5 RETURNING *;';
+
+		const { name_user, lastname, email, username, password, id_profile } =
+			input;
+
+		const client = await pool.connect();
+
+		try {
+			const userResult = await client.query(
+				'SELECT username FROM users WHERE id_user = $1',
+				[id]
+			);
+			const currentUsername = userResult.rows[0].username;
+			if (username && username !== currentUsername) {
+				const usernameExistsQuery =
+					'SELECT COUNT(*) FROM users WHERE username = $1';
+				const usernameExistsResult = await client.query(usernameExistsQuery, [
+					username,
+				]);
+				if (usernameExistsResult.rows[0].count > 0) {
+					throw new ClientError(
+						`User with username '${username}' already exists`,
+						400,
+						'Username already exists'
+					);
+				}
+			}
+
+			const result = await client.query(updateQuery, [
+				name_user,
+				lastname,
+				email,
+				id_profile,
+
+				id,
+			]);
+
+			if (result.rowCount === 0) {
+				// id invalido
+				// no se encontró ningún usuario con ese ID
+				throw new ClientError('Invalid ID', 404, 'No user found with that ID');
+			}
+
+			// else if (result.rows[0].username === username) {
+			// 	console.log(result.rows[0].username, username);
+			// 	// El usuario '' ya está registrado.
+			// 	// El nuevo username es igual al username existente, no es necesario actualizar
+			// 	throw new ClientError(
+			// 		`User with username '${username}' already exists`,
+			// 		400,
+			// 		'New username is the same as the existing one'
+			// 	);
+			// }
+
+			return {
+				message: 'User updated successfully',
+				updatedUser: true,
+				status: 200,
+				dataRecords: result.rows[0],
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				throw error;
+			} else if (error.code === '23505' && error.constraint === 'uk_users') {
+				// El usuario '' ya está registrado.
+				// ya existe el usuario. Por favor, intente con otro nombre de usuario
+				throw new ClientError(
+					`User with username '${username}' already exists`,
+					409,
+					'the usuario already exists. Please try another username'
+				);
+			} else {
+				throw new ServerError('error updating user', 500, '');
+			}
 		} finally {
 			await client.end();
 		}
@@ -79,14 +268,14 @@ export class UserModel {
 
 			if (result.rowCount === 0) {
 				throw new ClientError(
-					'invalid username',
+					'Invalid username',
 					404,
-					'no user was found with that username'
+					'No user was found with that username'
 				);
 			}
 
 			return {
-				message: 'welcome', // bienvenido
+				message: 'Welcome', // bienvenido
 				foundUser: true,
 				status: 200,
 				dataRecords: result.rows[0],
@@ -116,14 +305,14 @@ export class UserModel {
 
 			if (result.rowCount === 0) {
 				throw new ClientError(
-					'invalid ID',
+					'Invalid ID',
 					404,
-					'no user was found with that profile'
+					'No user was found with that profile'
 				);
 			}
 
 			return {
-				message: 'user successfully obtained',
+				message: 'User successfully obtained',
 				verifyUser: true,
 				status: 200,
 				dataRecords: result.rows[0],
