50cc1ce61a4aa908c75c01c7a26a41bd6c40c4d2
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-02 08:43:08 -0500

    feat: delete frontend with vuejs

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	darkMode: true,
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
