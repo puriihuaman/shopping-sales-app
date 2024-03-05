ac24d50d67c55227ee009e26f30e273e80b87d98
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-05 11:49:06 -0500

    feat: add customers router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 import express, { json } from 'express';
 import { PORT } from './config/config.js';
+import { corsMiddlewares } from './middlewares/cors.middleware.js';
+import { authRouter } from './routes/auth.routes.js';
+import { customerRouter } from './routes/customer.routes.js';
 import { profileRouter } from './routes/profile.routes.js';
 import { userRouter } from './routes/user.routes.js';
-import { authRouter } from './routes/auth.routes.js';
-import { corsMiddlewares } from './middlewares/cors.middleware.js';
 import { responseError } from './utils/index.js';
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(corsMiddlewares());
 app.use('/api/login', authRouter);
 app.use('/api/profiles', profileRouter);
 app.use('/api/users', userRouter);
+app.use('/api/customers', customerRouter);
 
 // + Manejando los errores
 app.use((error, req, res, next) => {
