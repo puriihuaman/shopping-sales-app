5ca8c78ae24dc2432f8b7502543ad3bd8067a307
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-06 12:11:37 -0500

    feat: add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,26 @@
+import { Sidebar } from '@components/Sidebar';
 import { Home } from '@views/Home';
 import { LoginView } from '@views/LoginView';
+import { Products } from '@views/Products';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
-			<BrowserRouter basename="/">
-				<Routes>
-					<Route path="/" element={<LoginView />} />
-					<Route path="/home" element={<Home />} />
-				</Routes>
-			</BrowserRouter>
+			<div className="flex">
+				<BrowserRouter basename="/">
+					<Sidebar />
+
+					<Routes>
+						<Route path="/" element={<LoginView />} />
+						<Route path="/compras" element={<Home />} />
+						<Route path="/ventas" element={<Home />} />
+						<Route path="/lista-compras" element={<Home />} />
+						<Route path="/lista-ventas" element={<Home />} />
+						<Route path="/productos" element={<Products />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
 		</>
 	);
 }
