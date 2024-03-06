6bd00262441163e186f7f2ce95d50f67ff586d09
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-06 12:11:58 -0500

    feat: add home view

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,7 +1,7 @@
 export const Home = () => {
 	return (
-		<main className="min-h-screen bg-slate-950 text-slate-50">
-			<h2>Home</h2>
+		<main className="flex-1 min-h-screen bg-slate-950 text-slate-50">
+			<h2>Compras</h2>
 		</main>
 	);
 };
