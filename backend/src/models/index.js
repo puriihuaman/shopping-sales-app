cdf1d25687ea945de377cdc4774c87baed0ee6ba
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-07 10:15:56 -0500

    feat: add provider model

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,5 +1,6 @@
 import { CustomerModel } from './customer.model.js';
 import { ProfileModel } from './profile.model.js';
+import { ProviderModel } from './provider.model.js';
 import { UserModel } from './user.model.js';
 
-export { CustomerModel, ProfileModel, UserModel };
+export { CustomerModel, ProfileModel, ProviderModel, UserModel };
