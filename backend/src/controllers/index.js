cb2226367dc9d4d873e7d228ec3aa9ce1fdb7902
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-07 10:17:04 -0500

    feat: add provider controller

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,5 +1,11 @@
 import { CustomerController } from './customer.controller.js';
 import { ProfileController } from './profile.controller.js';
+import { ProviderController } from './provider.controller.js';
 import { UserController } from './user.controller.js';
 
-export { CustomerController, ProfileController, UserController };
+export {
+	CustomerController,
+	ProfileController,
+	ProviderController,
+	UserController,
+};
