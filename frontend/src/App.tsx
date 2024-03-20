070f25cf6253f0fb68e1452ffee1a0b166c7ba77
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-20 12:51:55 -0500

    feat: add shopping at router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Sidebar } from '@components/Sidebar';
 import { Home } from '@views/Home';
 import { LoginView } from '@views/LoginView';
 import { Products } from '@views/Products';
+import { Shopping } from '@views/Shopping';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
 
 					<Routes>
 						<Route path="/" element={<LoginView />} />
-						<Route path="/compras" element={<Home />} />
+						<Route path="/compras" element={<Shopping />} />
 						<Route path="/ventas" element={<Home />} />
 						<Route path="/lista-compras" element={<Home />} />
 						<Route path="/lista-ventas" element={<Home />} />
