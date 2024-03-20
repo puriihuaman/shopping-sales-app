01086fcba0b195cbb2bb308852be148c7ce029a6
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-20 12:25:48 -0500

    fix: fix dark mode classes

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,13 +1,14 @@
 import { NavItem } from './NavItem';
+import ToggleTheme from './ToggleTheme';
 
 export const Sidebar = () => {
 	return (
-		<aside className="w-full max-w-64 min-h-screen bg-slate-900 text-slate-300">
-			<div className="bg-slate-900 py-4 flex justify-center items-center border-b border-slate-900">
+		<aside className="flex flex-col w-full max-w-64 min-h-screen text-slate-800 bg-slate-100 dark:bg-slate-900 dark:text-slate-300 transition-colors duration-300">
+			<div className="py-4 flex justify-center items-center border-b border-slate-300 dark:border-slate-800 transition-colors duration-300">
 				<img
 					src="/assets/images/logo.png"
 					alt="Logo"
-					className="w-14 h-14 invert"
+					className="w-14 h-14 dark:invert"
 				/>
 			</div>
 
@@ -28,6 +29,10 @@ export const Sidebar = () => {
 					<NavItem path="/productos" title="Productos" iconID="package" />
 				</ul>
 			</nav>
+
+			<div className="flex justify-center mt-auto mb-10">
+				<ToggleTheme />
+			</div>
 		</aside>
 	);
 };
